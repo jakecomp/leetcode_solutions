d2418f6ec54b084256bb090ca73f0f3212e21b95
jakecomp <jakobvalen9@gmail.com>
2023-06-20 16:00:12 -0700

    Add trivial python solution for needle in haystack problem - will try in c++ for more of a challenge

---
+++ python/needleInHaystack.py
@@ -0,0 +1,4 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int: 
+        # Very trivial in python, will implement in C++ for more of a challenge
+        return haystack.find(needle)
